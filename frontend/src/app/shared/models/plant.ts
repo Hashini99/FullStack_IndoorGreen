25ad82a6add6cb8e05411f91230b2d40cf567452
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2022-12-08 10:00:57 +0530

    Create plant.ts

---
+++ frontend/src/app/shared/models/plant.ts
@@ -0,0 +1,14 @@
+export class plant{
+  id!:string;
+  price!:number;
+  name!:string;
+  tags?:string[];
+  favorite!:boolean;
+ // stars!:number;
+  imageUrl!:string;
+
+  description!:string;
+  //origins!:string[];
+  //cookTime!:string;
+
+}
