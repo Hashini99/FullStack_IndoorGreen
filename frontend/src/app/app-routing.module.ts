32a5dd157e59474f9328021dfb0a9d52324eaa44
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2022-12-08 13:49:57 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PlantPageComponent } from './components/pages/plant-page/plant-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+ { path: 'search/:search_term', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  {path:'plant/:id', component:PlantPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
